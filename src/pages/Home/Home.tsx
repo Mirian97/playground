c2ea1c5d04724e05739faf4b48517a4106349070
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-22 05:25:59 -0300

    feat: create a home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,32 @@
+import reactLogo from "@/assets/svg/react.svg";
+import { useState } from "react";
+import viteLogo from "/svg/vite.svg";
+
+const Home: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+          className="m-5 bg-slate-500"
+        >
+          <img src={viteLogo} alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} alt="React logo" />
+        </a>
+      </div>
+      <h1>Wivenn Playground</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
