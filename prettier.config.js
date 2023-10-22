a0cbd2fd8fc51653d506b7440e5aec4b618ed09e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-22 05:23:20 -0300

    Initial setup

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// export const plugins = ["prettier-plugin-tailwindcss"];
+
+export default {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  trailingComma: "none",
+  semi: false,
+  arrowParens: "always",
+  printWidth: 90,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
