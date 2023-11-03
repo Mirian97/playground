f4338584a59efcf4bebeafe8dc3eb3c56bc1c0a2
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-03 15:14:47 -0300

    feat: wip jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,41 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
+  collectCoverage: true,
+  testEnvironment: "jsdom",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/*.{js,ts}",
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy", //lib que não precisa ser baixada pois está sendo usada por baixo dos panos pelo jest
+
+    // Handle CSS imports (without CSS modules)
+    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/coverage/"],
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
 };
 
 export default config;
