5743286b4170744386d50069a099601ba18439c3
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-04 12:45:44 -0300

    feat: create first test to home page

---
+++ src/pages/Home/Home.test.tsx
@@ -0,0 +1,21 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Home from "./Home";
+
+const renderHome = () => render(<Home />);
+
+describe("Home", () => {
+  it("should render Home page correctly", () => {
+    const { getByText, getByAltText, getByRole } = renderHome();
+
+    const WLogo = getByAltText(/vite logo/i);
+    const ReactLogo = getByAltText(/react logo/i);
+    const appTitle = getByText(/wivenn playground/i);
+    const counterButton = getByRole("button");
+
+    expect(WLogo).toBeInTheDocument();
+    expect(ReactLogo).toBeInTheDocument();
+    expect(appTitle).toBeInTheDocument();
+    expect(counterButton).toBeInTheDocument();
+  });
+});
