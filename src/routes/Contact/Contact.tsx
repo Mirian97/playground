3bb2ce46d2b59619134716bc4ee28943a969ae59
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-22 11:28:02 -0300

    feat: create page contact

---
+++ src/routes/Contact/Contact.tsx
@@ -0,0 +1,73 @@
+import TLoaderData from "@/@types/useLoaderData";
+import { TContactItem } from "@/helpers/contact/ContactTypes";
+import { Form, useLoaderData } from "react-router-dom";
+import { contactLoader } from "./loader";
+
+const Contact = () => {
+  const loaderData = useLoaderData() as TLoaderData<typeof contactLoader>;
+  if (loaderData?.contact === undefined) return null;
+  const { avatar, last, first, twitter, notes } = loaderData.contact;
+
+  return (
+    <div id="contact">
+      <div>{avatar !== undefined && <img key={avatar} src={avatar} />}</div>
+
+      <div>
+        <h1>
+          {first || last ? (
+            <>
+              {first} {last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={loaderData.contact} />
+        </h1>
+
+        {twitter && (
+          <p>
+            <a target="_blank" href={`https://twitter.com/${twitter}`}>
+              {twitter}
+            </a>
+          </p>
+        )}
+
+        {notes && <p>{notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (!confirm("Please confirm you want to delete this record.")) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Favorite = ({ contact }: { contact: TContactItem }) => {
+  const favorite = contact.favorite;
+  return (
+    <Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </Form>
+  );
+};
+
+export default Contact;
