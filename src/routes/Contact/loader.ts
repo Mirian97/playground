3385169ae4131462524eea1d63cf3872e24a95b5
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-22 10:37:46 -0300

    refactor: handle with types params

--- src/routes/Contact/loader.ts
+++ src/routes/Contact/loader.ts
@@ -3,11 +3,12 @@ import type { LoaderFunctionArgs } from "react-router-dom";
 
 interface IContactLoaderArgs extends LoaderFunctionArgs {
   params: {
-    contactId: string;
+    contactId?: string;
   };
 }
 
 export const contactLoader = async ({ params }: IContactLoaderArgs) => {
+  if (!params.contactId) return;
   const contact = await getContact(params.contactId);
   return { contact };
 };
