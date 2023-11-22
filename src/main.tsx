4bf205f0eb811bfceec58c083e70b802e136754d
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-22 10:40:43 -0300

    feat: create routes to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
+import ErrorPage from "@/pages/Error/Error";
+import Contact from "@/routes/Contact/Contact";
+import Root from "@/routes/Root/Root";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./pages/Home/Home.tsx";
+import { contactLoader } from "./routes/Contact/loader";
+import EditContact from "./routes/EditContact/EditContact";
+import { editContactAction } from "./routes/EditContact/loader";
+import { rootAction, rootLoader } from "./routes/Root/loader";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editContactAction,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
