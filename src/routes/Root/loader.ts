e66c2bd1bbe28262cf07d7ec7fb6af827f1c56c3
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-23 19:22:27 -0300

    feat: modify rootLoader to search contact by q params

--- src/routes/Root/loader.ts
+++ src/routes/Root/loader.ts
@@ -1,9 +1,12 @@
+import { IContactLoaderArgs } from "@/@types/ContactLoaderArgs";
 import { createContact, getContacts } from "@/helpers/contact/contact";
 import { redirect } from "react-router-dom";
 
-export const rootLoader = async () => {
-  const contacts = await getContacts();
-  return { contacts };
+export const rootLoader = async ({ request }: IContactLoaderArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q || "");
+  return { contacts, q };
 };
 
 export const rootAction = async () => {
