1f42ef4459201acb73d1b042a1c96ae4f255b0ad
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-23 19:04:53 -0300

    feat: implement fetch in input search

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,34 +1,44 @@
 import TLoaderData from "@/@types/useLoaderData";
+import { useEffect } from "react";
 import {
   Form,
   NavLink,
   Outlet,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from "react-router-dom";
 import { rootLoader } from "./loader";
 
 const Root = () => {
-  const { contacts } = useLoaderData() as TLoaderData<typeof rootLoader>;
+  const { contacts, q } = useLoaderData() as TLoaderData<typeof rootLoader>;
   const { state } = useNavigation();
   //state could be 'idle' | 'loading' | 'submitting'
+  const submit = useSubmit();
+
+  useEffect(() => {
+    const inputElement = document.getElementById("q") as HTMLInputElement;
+    inputElement.value = q || "";
+  }, [q]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q || ""}
+              onChange={(event) => submit(event.currentTarget.form)}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite" />
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
