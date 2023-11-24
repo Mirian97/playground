0f1f5d53aadff87d00a01ccd2f427dc7f7a7a717
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-24 10:51:19 -0300

    feat: render ErrorElement when occur an error

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import ErrorPage from "@/pages/Error/Error";
 import Contact from "@/routes/Contact/Contact";
+import { contactLoader, favoriteAction } from "@/routes/Contact/loader";
+import { deleteContactAction } from "@/routes/DeleteContact/loader";
+import EditContact from "@/routes/EditContact/EditContact";
+import { editContactAction } from "@/routes/EditContact/loader";
+import Index from "@/routes/Root/Index/Index";
 import Root from "@/routes/Root/Root";
+import { rootAction, rootLoader } from "@/routes/Root/loader";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import { contactLoader } from "./routes/Contact/loader";
-import { deleteContactAction } from "./routes/DeleteContact/loader";
-import EditContact from "./routes/EditContact/EditContact";
-import { editContactAction } from "./routes/EditContact/loader";
-import Index from "./routes/Root/Index/Index";
-import { rootAction, rootLoader } from "./routes/Root/loader";
 
 const router = createBrowserRouter([
   {
@@ -20,23 +20,29 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      { index: true, element: <Index /> },
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editContactAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        errorElement: <p>Oops! There was an error!</p>,
-        loader: contactLoader,
-        action: deleteContactAction,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: favoriteAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editContactAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            errorElement: <p>Oops! There was an error!</p>,
+            loader: contactLoader,
+            action: deleteContactAction,
+          },
+        ],
       },
     ],
   },
