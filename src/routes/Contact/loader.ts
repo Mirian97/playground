8b461f48a81427efa9f0a1df5aa3d7fea47be4db
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-11-24 10:47:42 -0300

    feat: create favorite action in contact route

--- src/routes/Contact/loader.ts
+++ src/routes/Contact/loader.ts
@@ -1,8 +1,25 @@
 import { IContactLoaderArgs } from "@/@types/ContactLoaderArgs";
-import { getContact } from "@/helpers/contact/contact";
+import { getContact, updateContact } from "@/helpers/contact/contact";
 
 export const contactLoader = async ({ params }: IContactLoaderArgs) => {
   if (!params.contactId) return;
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
+};
+
+export const favoriteAction = async ({
+  params,
+  request,
+}: IContactLoaderArgs) => {
+  const formData = await request.formData();
+  if (params.contactId === undefined) return;
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 };
