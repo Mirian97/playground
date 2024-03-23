275b48d6746eb1817b56cfb87d4e125f78cf16b1
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-03-23 17:29:11 -0300

    feat: configure storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jest-dom/recommended",
+    "plugin:storybook/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
