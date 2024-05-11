5bec7c8dacd457c6822b6ba3e93d0f6b3386ac89
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-05-11 14:10:58 -0300

    feat: enable react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import Index from "@/routes/Root/Index/Index";
 import Root from "@/routes/Root/Root";
 import { rootAction, rootLoader } from "@/routes/Root/loader";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -66,6 +67,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
